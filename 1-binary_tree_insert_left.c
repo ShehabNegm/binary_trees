dedd65d6f4d3fccbf454f8ebe2abf26763f7d93c
ShehabNegm <shebonegm@gmail.com>
2023-08-03 11:36:33 -0700

    fix compilation error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,18 +3,18 @@
 /**
   * binary_tree_insert_left - function to create a node of a parent in left
   * @parent: parent of the node to be created
-  * @value: value to be added to the node created
+  * @value: value to be added to the node created.
   * Return: will return pointer to new node or NULL if failed
   */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new, *tmp;
+
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
-	binary_tree_t *tmp;
-
+	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
 
