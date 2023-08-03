562ce41c49fc1ef7326ef4f58f3fe0ffab212c6d
ShehabNegm <shebonegm@gmail.com>
2023-08-02 17:11:26 -0700

    add function to add node to the right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,7 +4,7 @@
   * binary_tree_insert_right - function to create a node of a parent in right
   * @parent: parent of the node to be created
   * @value: value to be added to the node created
-  * Return: will return pointer to new node or NULL if failed
+  * Return: will return pointer to new node or NULL if failed.
   */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
