7b742bd7fcfa3c03a06d6bdf7c8c93b1def7978d
ShehabNegm <shebonegm@gmail.com>
2023-08-03 12:33:14 -0700

    fux checker no.6

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,6 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (tree->right == NULL && tree->left == NULL && tree->parent == NULL)
+		return (1);
+
 	while (tree)
 	{
 		if (tree->right == NULL)
